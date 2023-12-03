a24071de0b8f05700fcf1faf1d415bf03736db6c
Roshan Shibu <roshanshibu97@gmail.com>
2023-12-03 14:59:25 +0100

    Planning layout animation + styling

--- app/components/Planning/Planning.jsx
+++ app/components/Planning/Planning.jsx
@@ -1,12 +1,28 @@
-import "./Planning.css";
+"use client";
 
-const Planning = ({ isExpanded }) => {
+import { useState } from "react";
+import Image from "next/image";
+
+import "./Planning.css";
+import expandArrow from "@/assets/expand-arrow.svg";
+
+const Planning = () => {
+  const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <div class={`planning ${isExpanded ? "max" : ""}`}>
+    <div class={`planning ${isExpanded ? "max" : "min"}`}>
       <div class="styledSeparator">&nbsp;</div>
       <div className="planningMainContainer">
         <div class="iNeedContainer">
-          <p>I need ____</p>
+          <Image
+            className="expandArrow"
+            src={expandArrow}
+            onClick={() => {
+              setIsExpanded((previous) => {
+                return !previous;
+              });
+            }}
+          />
+          <p>I need</p>
         </div>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <div class="content"></div>
-      <Planning isExpanded={false} />
+      <Planning />
     </>
   );
 }
