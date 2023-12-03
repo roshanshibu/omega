294eb99e8321efb94617b4708a354588b0dd056a
Roshan Shibu <roshanshibu97@gmail.com>
2023-12-03 22:05:24 +0100

    test cams

--- app/components/SearchBar/SearchBar.jsx
+++ app/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,38 @@
 import "./SearchBar.css";
-import { useState } from "react";
+import { React, useEffect, useState } from "react";
 import { useZxing } from "react-zxing";
 
+const constraints = {
+  video: true,
+  audio: false,
+};
+
 const SearchBar = ({ onBarCodeScan }) => {
+  const [deviceId, setDeviceId] = useState(null);
+  const getList = () => {
+    navigator.mediaDevices.enumerateDevices().then((data) => {
+      console.log(data);
+      const availableVideoDevices = data.filter(
+        (device) => device.kind === "videoinput"
+      );
+      console.log(availableVideoDevices);
+      alert(`video device array length :${availableVideoDevices.length}`);
+      setDeviceId[
+        availableVideoDevices[availableVideoDevices.length - 1].deviceId
+      ];
+    });
+  };
+
+  useEffect(() => {
+    getList();
+  });
+
   const { ref } = useZxing({
     onDecodeResult(result) {
       onBarCodeScan(result.getText());
       console.log(result.getText());
     },
+    deviceId,
   });
 
   return (
